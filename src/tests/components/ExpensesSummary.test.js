4157e052cc4168e14622e5f2d5bdc1d774bf1002
pericles <pericles3d@gmail.com>
2018-05-17 16:34:56 -0700

    Add expense summary to dashboard

---
+++ src/tests/components/ExpensesSummary.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExpensesSummary } from '../../components/ExpensesSummary';
+
+test('Should correctly render expenses summary with 1 expense', () => {
+    const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={235} />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should correctly render expenses summary with multiple expenses', () => {
+    const wrapper = shallow(<ExpensesSummary expenseCount={23} expensesTotal={2357897} />);
+    expect(wrapper).toMatchSnapshot();
+});
+// test('', () => {
+
+// });
